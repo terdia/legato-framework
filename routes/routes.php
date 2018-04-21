9ed3b6b84e9a5a58ce4e7eabf71e7a4f9087c0fd
Devscreencast <support@devscreencast>
2018-04-21 15:30:25 +0800

    Add blade template support and using config for twig global

--- routes/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $router){
-    $router->addRoute('GET', '/', 'Legato\Framework\Controllers\IndexController@show');
-});
-
-return $dispatcher;
--- src/TwigGlobal.php
+++ src/TwigGlobal.php
@@ -6,14 +6,38 @@ use Legato\Framework\Session\SessionManager;
 class TwigGlobal extends AbstractTwigGlobal
 {
     use SessionManager;
+    protected $config = [];
 
     public function getGlobals()
     {
+        return array_merge($this->getDefaults(), $this->getUserDefinedGlobals());
+    }
+    
+    /**
+     * get the default globals available for twig template
+     * @return array
+     */
+    public function getDefaults()
+    {
         return [
-          'flash' => $this->getFlashMessage('flash'),
-          'success' => $this->get('success'),
-          'validation_errors' => [],
-          'error' => $this->get('error'),
+            'flash' => $this->getFlashMessage('flash'),
+            'success' => $this->get('success'),
+            'validation_errors' => [],
+            'error' => $this->get('error'),
         ];
+    }
+    
+    /**
+     * Get the user defined global for twig templates
+     * @return mixed
+     */
+    public function getUserDefinedGlobals()
+    {
+        if(file_exists(realpath(__DIR__.'/../../../../../config/twig.php'))){
+            $this->config = require_once realpath(__DIR__.'/../../../../../config/twig.php');
+        }elseif (file_exists(realpath(__DIR__ . '/../config/twig.php'))){
+            $this->config = require_once realpath(__DIR__ . '/../config/twig.php');
+        }
+        return $this->config['twig_global'];
     }
 }
