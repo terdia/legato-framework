0d593be06eb0b0ec322e1e8d84787783f214f6ea
Osayawe Ogbemudia Terry <terrymarcy2000@yahoo.com>
2018-05-29 05:34:49 +0800

    empty cookie class

--- src/Cookie/Cookie.php
+++ src/Cookie/Cookie.php
@@ -7,22 +7,5 @@ use Symfony\Component\HttpFoundation\Cookie as SymfonyCookie;
 
 class Cookie extends SymfonyCookie
 {
-    protected static $instance;
 
-    /**
-     * Cookie Instance
-     * @param $name
-     * @param $value
-     * @param int $expire
-     * @return mixed
-     */
-    public static function getInstance($name, $value, $expire, $path, $domain, $secure, $httpOnly, $raw, $sameSite)
-    {
-        if( $expire == 0 ) $expire = time() +2592000;
-        if(! static::$instance instanceof Cookie )
-        {
-            static::$instance = new Cookie($name, $value, $expire, $path, $domain, $secure, $httpOnly, $raw, $sameSite);
-        }
-        return static::$instance;
-    }
 }
